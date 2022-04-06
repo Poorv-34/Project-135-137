ca33ef83c1e9ecda57a9b21db4d66dc4d63c5c80
Poorv-34 <80013101+Poorv-34@users.noreply.github.com>
2022-04-06 18:28:04 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+objects = [];
 
 function setup(){
         canvas = createCanvas(480, 380);
@@ -19,6 +20,30 @@ object_detector = ml5.objectDetector('cocossd', modelLoaded);
      status == true;
  }
 
+ function gotResults(error, results){
+objects = results;
+ }
+
  function draw(){
      video.show();
+     if(status != ""){
+        percent = floor(objects[i].confidence * 100);
+        text(objects[i].label + " " + percent + "% ", objects[i].x + 15, objects[i].y + 15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+         
  }
+
+ if (objects[i].label == status){
+     video.stop();
+     object_detector.detect(gotResults);
+     document.getElementById("object_status").innerHTML = status + " Found";
+     var synth = window.speechSynthesis;
+    var utterThis = new SpeechSynthesisUtterance("object mentioned found");
+    synth.speak(utterThis);
+ }
+ else{
+     document.getElementById("object_status").innerHTML = "Object Mentioned Not Found";
+ }
+}
